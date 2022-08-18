a680fadabfdeb3c24c9c4f9ad6a4510a2dbe32f8
山下純一 <metarusword1998@gmail.com>
2022-08-19 08:23:53 +0900

    地図マーカー追加

--- src/components/test-map-box/test-map-box.tsx
+++ src/components/test-map-box/test-map-box.tsx
@@ -7,6 +7,7 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { getApp, FirebaseApp } from 'firebase/app';
+import { CurrentCoordinate } from './test-map-box.style';
 
 const mapStyle: mapboxgl.Style = {
   version: 8,
@@ -32,6 +33,9 @@ const mapStyle: mapboxgl.Style = {
 
 export const TestMapBox: FunctionComponent = () => {
   const [mapInstance, setMapInstance] = useState<mapboxgl.Map>();
+  const [currentLng, setCurrentLng] = useState<number>(142.0);
+  const [currentLat, setCurrentLat] = useState<number>(40.0);
+  const [currentZoom, setCurrentZoom] = useState<number>(4);
   const mapContainer = useRef<HTMLDivElement | null>(null);
   const app: FirebaseApp = getApp();
 
@@ -43,12 +47,83 @@ export const TestMapBox: FunctionComponent = () => {
       container: mapContainer.current, // ていねいな型ガードのおかげで必ずHTMLDivElementとして扱える、current!でも可
       accessToken: process.env.NEXT_PUBLIC_MAPBOX_TOKEN,
       style: mapStyle,
-      center: [142.0, 40.0],
-      zoom: 4,
+      center: [currentLng, currentLat],
+      zoom: currentZoom,
     });
     // mapboxgl.Mapのインスタンスへの参照を保存
     setMapInstance(map);
   }, []);
+
+  useEffect(() => {
+    if (!mapContainer.current) return;
+
+    if (mapInstance) {
+      mapInstance.on('move', () => {
+        setCurrentLng(Number(mapInstance.getCenter().lng.toFixed(4)));
+        setCurrentLat(Number(mapInstance.getCenter().lat.toFixed(4)));
+        setCurrentZoom(Number(mapInstance.getZoom().toFixed(2)));
+      });
+    }
+  }, [mapInstance]);
+
+  const markers = [
+    {
+      city: 'Kenninji',
+      country: 'Japan',
+      latCoord: 35.00028883396185,
+      longCoord: 135.77350069157626,
+      color: 'red',
+    },
+    {
+      city: 'NaZenji',
+      country: 'Japan',
+      latCoord: 35.0107325368728,
+      longCoord: 135.7940018972439,
+      color: 'pink',
+    },
+    {
+      city: 'Shimogamojinja',
+      country: 'Japan',
+      latCoord: 35.0397503053051,
+      longCoord: 135.7691401418707,
+      color: 'blue',
+    },
+  ];
+
+  const geojson = {
+    type: 'Feature',
+    features: markers.map((marker) => ({
+      geometry: {
+        type: 'Point',
+        coordinates: {
+          lat: marker.latCoord,
+          lng: marker.longCoord,
+        },
+      },
+      properties: {
+        color: marker.color,
+      },
+    })),
+  };
+
+  useEffect(() => {
+    if (mapInstance) {
+      mapInstance.on('load', () => {
+        geojson.features.forEach((marker) => {
+          // create a DOM element for the marker
+          const markerIcon = document.createElement('div');
+          markerIcon.className = 'location-marker';
+          markerIcon.style.backgroundImage = `url(/marker-icons/mapbox-marker-icon-20px-${marker.properties.color}.png)`;
+          markerIcon.style.width = '20px';
+          markerIcon.style.height = '30px';
+          markerIcon.style.cursor = 'pointer';
+          new mapboxgl.Marker(markerIcon)
+            .setLngLat(marker.geometry.coordinates)
+            .addTo(mapInstance);
+        });
+      });
+    }
+  }, [mapInstance]);
 
   return (
     <div>
@@ -78,7 +153,12 @@ export const TestMapBox: FunctionComponent = () => {
           <Button size='small'>Learn More</Button>
         </CardActions>
       </Card>
-      <div style={{ height: 800 }} ref={mapContainer} />
+      <div>
+        <CurrentCoordinate>
+          Longitude: {currentLng} | Latitude: {currentLat} | Zoom: {currentZoom}
+        </CurrentCoordinate>
+        <div style={{ height: 800 }} ref={mapContainer} />
+      </div>
     </div>
   );
 };
