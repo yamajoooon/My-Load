051def5cf4ee541697847f7d112c67cefbbeceaa
山下純一 <metarusword1998@gmail.com>
2022-08-22 04:16:01 +0900

    記事一覧ページファイル追加

---
+++ src/components/posts/posts.tsx
@@ -0,0 +1,13 @@
+import { FunctionComponent } from 'react';
+import { Container } from './posts.style';
+
+export const Posts: FunctionComponent = () => {
+  return (
+    <Container>
+      <div>
+        <div>こんばんは</div>
+        <div>こんにちは</div>
+      </div>
+    </Container>
+  );
+};
